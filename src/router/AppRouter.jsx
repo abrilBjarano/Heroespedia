69e753c1b5074ace07c6cd605f41c5f81f454cfd
abrilxcx <abrlbjrn@gmail.com>
2024-06-25 13:29:46 -0600

    Changed routes structure in AppRouter and Navbar

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -10,15 +10,20 @@ import { Navbar } from "../ui";
 export const AppRouter = () => {
 
 
+    const routes = [
+      { path: '/marvel', name: 'Marvel' },
+      { path: '/dc', name: 'DC' },
+    ]
+
+
     return (
       <>
 
-        <Navbar />
+        <Navbar routes={ routes } />
 
         <Routes>
           <Route path="marvel" element={ <MarvelPage/> } />
           <Route path="dc" element={ <DCPage /> } />
-
           <Route path="login" element={ <LoginPage /> } />
 
           <Route path="/*" element={ <Navigate to='marvel' /> } />
--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,43 +1,39 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 
-export const Navbar = () => {
+export const Navbar = ({ routes }) => {
+
+    const location = useLocation();
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
             
-            <a 
-                className="navbar-brand" 
-            >
-                Asociaciones
-            </a>
+            <a className="navbar-brand"> Asociaciones </a>
 
             <div className="navbar-collapse">
                 <div className="navbar-nav">
 
-                    <NavLink 
-                        className="nav-item nav-link" 
-                        to="/marvel"
-                    >
-                        Marvel
-                    </NavLink>
-
-                    <NavLink 
-                        className="nav-item nav-link" 
-                        to="/dc"
-                    >
-                        DC
-                    </NavLink>
+                    {
+                        routes.map( route => (
+                            <Link 
+                                className={`nav-link ${ location.pathname === route.path ? 'active' : '' }`} 
+                                key={ route.path }
+                                to={ route.path }
+                                >
+                                { route.name }
+                            </Link>
+                    ))}
                 </div>
             </div>
 
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
                 <ul className="navbar-nav ml-auto">
-                    <NavLink 
+                    <Link 
                         className="nav-item nav-link" 
                         to="/login"
                     >
                         Logout
-                    </NavLink>
+                    </Link>
                 </ul>
             </div>
         </nav>
