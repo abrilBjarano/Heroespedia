9bc858b16ddc374926f0e048603940326f5de27a
abrilxcx <abrlbjrn@gmail.com>
2024-06-25 11:36:00 -0600

    Imported React Router stuff in main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import './styles.css';
 import { Heroespedia } from './Heroespedia';
 
+
+const router = createBrowserRouter([
+  {
+    path: ".",
+    element: <Heroespedia />,
+  },
+]);
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Heroespedia />
+    <RouterProvider router={ router } />
   </React.StrictMode>,
 )
