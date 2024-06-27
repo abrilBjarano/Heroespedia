31036be2a13cbaa2d94128a5e2a997a070224ba5
abrilxcx <abrlbjrn@gmail.com>
2024-06-27 10:00:09 -0600

    Improved DCPage and MarvelPage in relatiton to HeroList

--- src/heroes/pages/DCPage.jsx
+++ src/heroes/pages/DCPage.jsx
@@ -2,13 +2,11 @@ import { HeroList } from "../components/HeroList";
 
 export const DCPage = () => {
 
-   const publisher = 'DC Comics';
-
    return (
       <>
          <h1>DC Page</h1>
          <hr />
-         <HeroList publisher={ publisher } />
+         <HeroList publisher={ 'DC Comics' } />
       </>
    )
 }
--- src/heroes/pages/MarvelPage.jsx
+++ src/heroes/pages/MarvelPage.jsx
@@ -1,7 +1,12 @@
+import { HeroList } from "../components/HeroList";
+
 export const MarvelPage = () => {
+
    return (
       <>
-         <h1>MarvelPage</h1>
+         <h1>Marvel Page</h1>
+         <hr />
+         <HeroList publisher={ 'Marvel Comics' } />
       </>
    )
 }
