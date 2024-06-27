ebd412f180280a48951a9fcc74ac3540fa950b37
abrilxcx <abrlbjrn@gmail.com>
2024-06-27 10:03:24 -0600

    Improved Navbar padding text
    And added README to .gitignore (temporaly)

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -15,7 +15,7 @@ export const Navbar = ({ routes = [] }) => {
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
             
-            <a className="navbar-brand text-success pe-5"> Asociaciones </a>
+            <a className="navbar-brand text-success pe-5 ps-3"> Asociaciones </a>
 
             <div className="navbar-collapse">
                 <div className="navbar-nav">
@@ -33,7 +33,7 @@ export const Navbar = ({ routes = [] }) => {
                 </div>
             </div>
 
-            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
+            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end ps-3">
                 <ul className="navbar-nav ml-auto">
                     <span className="nav-item nav-link text-info">
                         Abril
