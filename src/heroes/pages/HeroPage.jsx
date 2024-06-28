b0463d509d311844fac7117377aa011451b457b5
abrilxcx <abrlbjrn@gmail.com>
2024-06-28 13:32:54 -0600

    Created getHeroesById() and implemented in HeroPage
    And added  to the helpers barrel file

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,4 +1,16 @@
+import { Navigate, useParams } from "react-router-dom"
+import { getHeroesById } from "../helpers";
+
 export const HeroPage = () => {
+
+  const { id, ...rest } = useParams();
+
+  const hero = getHeroesById( id );
+
+  if( !hero ) {
+    return <Navigate to="/" />
+  }
+
   return (
     <>
       <h1>HeroPage</h1>
