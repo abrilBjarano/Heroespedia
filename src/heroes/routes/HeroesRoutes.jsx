e7ac567ca807e4b00f4ef5199574b6066399ae49
abrilxcx <abrlbjrn@gmail.com>
2024-06-28 12:48:46 -0600

    ⭐ Removed wildcard from HeroesRoutes

--- src/heroes/routes/HeroesRoutes.jsx
+++ src/heroes/routes/HeroesRoutes.jsx
@@ -21,7 +21,7 @@ export const HeroesRoutes = () => {
                <Route path="search" element={ <SearchPage /> } />
                <Route path="hero" element={ <HeroPage /> } />
 
-               <Route path="/*" element={ <Navigate to='marvel' /> } />
+               <Route path="/" element={ <Navigate to='marvel' /> } />
             </Routes>
          </div>
       </>
