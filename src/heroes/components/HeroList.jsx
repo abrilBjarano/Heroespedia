9ff774dc53d9681733a700a62d046446dffe057e
abrilxcx <abrlbjrn@gmail.com>
2024-06-28 11:39:35 -0600

    Added useMemo into HeroList

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,9 +1,13 @@
+import { useMemo } from "react";
 import { getHeroesByPublisher } from "../helpers";
 import { HeroCard } from "./";
 
 export const HeroList = ({ publisher }) => {
 
-   const heroList = getHeroesByPublisher( publisher );
+   // const heroList = getHeroesByPublisher( publisher );
+   const heroList = useMemo( () => getHeroesByPublisher( publisher ), [ publisher ]);
+
+   console.log('Me volví a generar');
 
    return (
       <div className="row rows-cols-1 row-cols-md-1 g-3">
