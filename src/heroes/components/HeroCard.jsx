2cbb7a722e1029444dd33d149d44c4cfe0cf0cc9
abrilxcx <abrlbjrn@gmail.com>
2024-06-28 11:59:42 -0600

    🧑‍🎨 Added characters and centered them in the card

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -10,15 +10,19 @@ export const HeroCard = ({
   const heroImageUrl = `/assets/heroes/${ id }.jpg`;
 
   return (
-    <div className="col">
+    <div className="col mb-2">
       <div className="card h-100">
         <div className="row no-gutters h-100">
+
           <div className="col-2">
             <img src={ heroImageUrl } alt={ superhero } className="card-img h-100" />
           </div>
-          <div className="col-10 d-flex justify-content-center align-items-center">
-            <h1>{superhero}</h1>
+
+          <div className="col-10 d-flex flex-column justify-content-center align-items-center text-center p-3">
+            <h1 className="mb-3">{superhero}</h1>
+            <p className="text-wrap">{characters}</p>
           </div>
+
         </div>
       </div>
     </div>
