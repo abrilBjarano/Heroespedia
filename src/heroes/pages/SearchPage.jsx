5f81ad4f94a29c1b6adebef2df5b98d6c9a2cd41
abrilxcx <abrlbjrn@gmail.com>
2024-06-29 12:35:50 -0600

    Created SearchPage template

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,8 +1,42 @@
+import { HeroCard } from "../components/HeroCard";
+
 export const SearchPage = () => {
-   return (
-     <>
-       <h1>SearchPage</h1>
-     </>
-   )
- }
- 
+  return (
+    <>
+      <div className="row  mt-3">
+
+        <div className="col-5">
+          <form>
+            <input
+              type="text"
+              placeholder="Search a hero"
+              className="form-control"
+              name="searchText"
+              autoComplete="off"
+            />
+          </form>
+
+          <div className="btn btn-primary mt-2">
+            Search
+          </div>
+        </div>
+
+        <div className="col-7">
+          <h4>Results</h4>
+          <hr/>
+
+          <div className="alert alert-primary">
+            Search a hero
+          </div>
+          <div className="alert alert-danger">
+            No hero with <b>ABC</b>
+          </div>
+
+          {/* <HeroCard /> */}
+
+        </div>
+
+      </div>
+    </>
+  )
+}
