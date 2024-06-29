2dcd2b6f953f8cd46fbc57aaecb1522e4ca895b5
abrilxcx <abrlbjrn@gmail.com>
2024-06-29 13:10:59 -0600

    Created useForm

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,18 +1,27 @@
+import { useNavigate } from "react-router-dom";
+import { useForm } from "../../hooks/useForm";
 import { HeroCard } from "../components/HeroCard";
 
 export const SearchPage = () => {
+
+  // const navigate = useNavigate();
+  const { onInputChange, onSubmit, inputValue } = useForm();
+
+
   return (
     <>
-      <div className="row  mt-3">
+      <div className="row mt-3">
 
         <div className="col-5">
-          <form>
+          <form onSubmit={ onSubmit }>
             <input
               type="text"
               placeholder="Search a hero"
               className="form-control"
               name="searchText"
               autoComplete="off"
+              value={ inputValue }
+              onChange={ onInputChange }
             />
           </form>
 
---
+++ src/hooks/useForm.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+export const useForm = () => {
+
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    console.log({ location });
+
+    const [ inputValue, setInputValue ] = useState('');
+
+    const onInputChange = ({ target }) => {
+        setInputValue( target.value );
+    };
+
+    const onSubmit = ( event ) => {
+        event.preventDefault();
+        if ( inputValue.trim().length === 0 ) return;
+
+        navigate(`?q=${ inputValue.toLocaleLowerCase().trim() }`);
+        setInputValue('');
+    };
+
+    return {
+        onInputChange,
+        onSubmit,
+        inputValue
+    }
+}
