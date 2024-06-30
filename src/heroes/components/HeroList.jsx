25607d5c1e587c448618b65bfd06306882cfea1a
abrilxcx <abrlbjrn@gmail.com>
2024-06-30 12:13:48 -0600

    Made minor update in HeroList

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -6,8 +6,7 @@ export const HeroList = ({ publisher }) => {
 
    const heroList = useMemo( () => getHeroesByPublisher( publisher ), [ publisher ]);
 
-   console.log('Me volví a generar');
-
+   
    return (
       <div className="row rows-cols-1 row-cols-md-1 g-3 mt-2">
          { heroList.map( hero => (
